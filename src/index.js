a231a2bd59712171872e1b243c97cef28853b5e4
Bruno Ferreira Cangussu <bruno.cangussu@hotmail.com>
2018-06-11 23:05:04 -0300

    Setup for local data bases

--- src/index.js
+++ src/index.js
@@ -1,7 +1,4 @@
 import {Table1} from './schemas';
-import {mysql} from './connections';
 import scripts from '../scripts';
 console.log(scripts);
-let a = new Date();
-Table1.find({})
-    .then((...args) => console.log(new Date() - a, args));
+Table1.find({}, console.log);
--- src/schemas/table1.js
+++ src/schemas/table1.js
@@ -1,20 +1,11 @@
 import mongoose from 'mongoose';
 import explain from 'mongoose-explain';
-
+mongoose.set('debug', console.log);
 const Table1Schema = new mongoose.Schema({
-    name: String,
     createdAt: {
         type: Date,
         default: Date.now,
     },
-});
-
-Table1Schema.plugin(explain, {
-    callback: (stats) => {
-        stats.forEach(stat => {
-            console.log(stat.executionStats.executionTimeMillis);
-        });
-    }
 });
 
 const model = mongoose.model('table1', Table1Schema);
